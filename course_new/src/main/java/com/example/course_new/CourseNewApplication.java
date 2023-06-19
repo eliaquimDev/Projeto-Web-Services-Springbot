34396a951e280cda0000d0cc71747404d830f9ed
Eliaquim Rodriguês Ferreira <eliaquimdg@gmail.com>
2023-06-19 13:44:16 -0300

    Project created

---
+++ course_new/src/main/java/com/example/course_new/CourseNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.course_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseNewApplication.class, args);
+	}
+
+}
---
+++ course_new/src/test/java/com/example/course_new/CourseNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
