082b25e3b93d35a6fc60ea7311df1f841fa317ec
Eliaquim Rodriguês Ferreira <eliaquimdg@gmail.com>
2023-06-22 13:45:02 -0300

    Exercicios Concluidos

---
+++ Eliaquim-Microservices/02-calculator/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,61 @@
+package br.com.erudio.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.model.Person;
+
+@Service
+public class PersonServices {
+	
+	private final AtomicLong counter = new AtomicLong();
+	
+	public Person create(Person person) {
+		return person;
+		
+	}
+	public Person update(Person person) {
+		return person;
+		
+	}
+	public void delete(String id) {
+		
+		
+	}
+	public Person findByID(String id) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Eliaquim");
+		person.setLastName("Ferreira");
+		person.setAddress("Londrina - Pârana - Brasil");
+		person.setGender("Male");
+		return person;
+		
+	}
+	
+	public List<Person> findAll(){
+		List<Person> persons = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+			
+		}
+		return persons;
+		
+		
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name" + i);
+		person.setLastName("Last name");
+		person.setAddress("Some address in Brasil" + i);
+		person.setGender("Male");
+		return person;
+	}
+	
+}
